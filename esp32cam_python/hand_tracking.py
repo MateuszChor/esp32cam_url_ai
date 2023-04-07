e94587279d897dacdd3296e450bc30cdd964d80e
unknown <bitpit11@wp.pl>
2023-04-07 18:20:46 +0200

    make hand track model as object&get video from esp as func

--- esp32cam_python/hand_tracking.py
+++ esp32cam_python/hand_tracking.py
@@ -9,51 +9,74 @@ import time
 BASE_DIR = Path(__file__).absolute().parent
 path_to_modules = os.path.join(BASE_DIR, "modules")
 
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, model_complexity=1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.model_complexity = model_complexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.model_complexity,
+                                        self.detectionCon, self.trackCon)  # parameters of model
+        self.mpDraw = mp.solutions.drawing_utils  # drawing line tool
+    def findHands(self, img, draw=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+
+        # results.multi_hand_landmarks # <-- landmarks
+        # print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id_number, lm, in enumerate(handLms.landmark):
+                    if draw:
+                        self.mpDraw.draw_landmarks(img, handLms,
+                                              self.mpHands.HAND_CONNECTIONS)
+                        # # print(id_number, lm)
+                        # h, w, c = img.shape
+                        # cx, cy = int(lm.x * w), int(lm.y * h)
+                        # print(id_number, cx, cy)
+                        # if id_number == 4:  # draw circle for id landmark
+                        #     cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+        return img
+
+
+
+def get_video_from_esp32(url):
+    pTime = 0
+    cTime = 0
+
+    detector = handDetector()
+
+    while True:
+        # success, img = cap.read() # read from  camera hardware
+        url_img = urllib.request.urlopen(url)  # open address url with url to camera
+
+        img_np = np.array(bytearray(url_img.read()), dtype=np.uint8)  # on matrix
+        img = cv2.imdecode(img_np, -1)  # cv2 flag -1 encoding original format
+
+        detector.findHands(img)
+
+        # calculate fps
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+
+        cv2.imshow('Esp32Cam', img)
+        key = cv2.waitKey(5)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+
+
+
 url = 'http://192.168.8.116/cam-hi.jpg'
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()  # default parameters of model
-mpDraw = mp.solutions.drawing_utils  # drawing line tool
-
-pTime = 0
-cTime = 0
-
-while True:
-    # success, img = cap.read() # read from  camera hardware
-    url_img = urllib.request.urlopen(url)  # open address url with url to camera
-
-    img_np = np.array(bytearray(url_img.read()), dtype=np.uint8)  # on matrix
-    img = cv2.imdecode(img_np, -1)  # cv2 flag -1 encoding original format
-
-
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    # results.multi_hand_landmarks # <-- landmarks
-    # print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id_number, lm, in enumerate(handLms.landmark):
-                # print(id_number, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y*h)
-                print(id_number, cx, cy)
-                if id_number == 4:  # draw circle for id landmark
-                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
-    # calculate fps
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
-
-    cv2.imshow('Esp32Cam', img)
-    key = cv2.waitKey(5)
-    if key == ord('q'):
-        break
-
-cv2.destroyAllWindows()
+get_video_from_esp32(url)
