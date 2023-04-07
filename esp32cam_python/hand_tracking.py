24dbe6d17b68af198ec62f12b424330ffae27c32
unknown <bitpit11@wp.pl>
2023-04-07 16:59:46 +0200

    add drawing method

--- esp32cam_python/hand_tracking.py
+++ esp32cam_python/hand_tracking.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from pathlib import Path
 import mediapipe as mp
+import time
 
 BASE_DIR = Path(__file__).absolute().parent
 path_to_modules = os.path.join(BASE_DIR, "modules")
@@ -14,6 +15,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()  # default parameters of model
 mpDraw = mp.solutions.drawing_utils  # drawing line tool
 
+pTime = 0
+cTime = 0
 
 while True:
     # success, img = cap.read() # read from  camera hardware
@@ -31,8 +34,22 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            for id_number, lm, in enumerate(handLms.landmark):
+                # print(id_number, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y*h)
+                print(id_number, cx, cy)
+                if id_number == 4:  # draw circle for id landmark
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    # calculate fps
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
 
     cv2.imshow('Esp32Cam', img)
     key = cv2.waitKey(5)
